5d86ebb82c7e9303c6ab4a6d30fffe7333c008ef
Ovv <contact@ovv.wtf>
2019-05-05 13:47:30 +0200

    Postgresql migration on platformsh deploy hook

--- sirbot-pyslackers/__main__.py
+++ sirbot-pyslackers/__main__.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import logging.config
+import platformshconfig
 
 import yaml
 import raven
 from sirbot import SirBot
-from raven.conf import setup_logging
 from raven.processors import SanitizePasswordsProcessor
 from sirbot.plugins.slack import SlackPlugin
 from sirbot.plugins.github import GithubPlugin
@@ -21,33 +21,71 @@ PORT = os.environ.get("SIRBOT_PORT", os.environ.get("PORT", 9000))
 HOST = os.environ.get("SIRBOT_ADDR", "127.0.0.1")
 VERSION = "0.0.11"
 LOG = logging.getLogger(__name__)
+PSH_CONFIG = platformshconfig.Config()
 
 
-def make_sentry_logger():
+def make_sentry_logger(dsn):
+
+    if PSH_CONFIG.is_valid_platform():
+        version = PSH_CONFIG.treeID
+    else:
+        version = VERSION
+
     client = raven.Client(
-        dsn=os.environ["SENTRY_DSN"],
-        release=os.environ["SIRBOT_VERSION"],
+        dsn=dsn,
+        release=version,
         processor=SanitizePasswordsProcessor,
     )
     handler = SentryHandler(client)
     handler.setLevel(logging.WARNING)
-    setup_logging(handler)
+    raven.conf.setup_logging(handler)
 
 
-if __name__ == "__main__":
-
+def setup_logging():
     try:
         with open(
             os.path.join(os.path.dirname(os.path.realpath(__file__)), "../logging.yml")
         ) as log_configfile:
-            logging.config.dictConfig(yaml.load(log_configfile.read()))
+            logging.config.dictConfig(yaml.safe_load(log_configfile.read()))
 
     except Exception as e:
         logging.basicConfig(level=logging.DEBUG)
         LOG.exception(e)
 
-    if "SENTRY_DSN" in os.environ:
-        make_sentry_logger()
+    sentry_dsn = os.environ.get("SENTRY_DSN")
+    if sentry_dsn:
+        make_sentry_logger(sentry_dsn)
+
+
+def configure_postgresql_plugin():
+
+    if "POSTGRES_DSN" in os.environ:
+        dsn = os.environ["POSTGRES_DSN"]
+    elif PSH_CONFIG.is_valid_platform():
+        dsn = PSH_CONFIG.formatted_credentials("database", "postgresql_dsn")
+    else:
+        dsn = None
+
+    if dsn:
+        return PgPlugin(
+            version=VERSION,
+            sql_migration_directory=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "../sql"
+            ),
+            dsn=dsn,
+        )
+    else:
+        raise RuntimeError("No postgresql configuration available. Use POSTGRES_DSN environment variable")
+
+
+if __name__ == "__main__":
+
+    setup_logging()
+
+    if sys.argv[1] == "migrate":
+        postgres = configure_postgresql_plugin()
+        postgres.migrate()
+        sys.exit(0)
 
     bot = SirBot()
 
@@ -76,24 +114,7 @@ if __name__ == "__main__":
     endpoints.readthedocs.register(readthedocs)
     bot.load_plugin(readthedocs)
 
-    if "POSTGRES_DSN" in os.environ:
-        dsn = os.environ["POSTGRES_DSN"]
-    elif "PLATFORM_PROJECT" in os.environ:
-        import platformshconfig
-
-        config = platformshconfig.Config()
-        dsn = config.formatted_credentials("database", "postgresql_dsn")
-    else:
-        dsn = None
-
-    if dsn:
-        postgres = PgPlugin(
-            version=VERSION,
-            sql_migration_directory=os.path.join(
-                os.path.dirname(os.path.realpath(__file__)), "../sql"
-            ),
-            dsn=dsn,
-        )
-        bot.load_plugin(postgres)
+    postgres = configure_postgresql_plugin()
+    bot.load_plugin(postgres)
 
     bot.start(host=HOST, port=PORT, print=False)
